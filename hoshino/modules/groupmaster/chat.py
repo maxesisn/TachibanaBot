0e266f2621cfa34860849c44357a6352b3de9b23
Maximilian Wu <me@maxesisn.online>
2020-09-20 11:11:47 +0800

    chat代码格式化

--- hoshino/modules/groupmaster/chat.py
+++ hoshino/modules/groupmaster/chat.py
@@ -17,6 +17,7 @@ async def say_hello(session):
 
 sv = Service('chat', visible=False)
 
+
 @sv.on_fullmatch('沙雕机器人')
 async def say_sorry(bot, ev):
     await bot.send(ev, 'ごめんなさい！嘤嘤嘤(〒︿〒)')
@@ -65,6 +66,7 @@ async def chat_queshi(bot, ctx):
     if random.random() < 0.05:
         await bot.send(ctx, R.img('确实.jpg').cqcode)
 
+
 @sv.on_keyword(('内鬼'))
 async def chat_neigui(bot, ctx):
     if random.random() < 0.10:
@@ -76,21 +78,26 @@ nyb_player = f'''{R.img('newyearburst.gif').cqcode}
 ⇆ ㅤ◁ ㅤㅤ❚❚ ㅤㅤ▷ ㅤ↻
 '''.strip()
 
+
 @sv.on_keyword(('春黑', '新黑'))
 async def new_year_burst(bot, ev):
     if random.random() < 0.02:
         await bot.send(ev, nyb_player)
 
+
 @sv.on_rex(r'^我想要(\d+\.?\d*)元$')
-async def chat_alipay(bot,ev):
-    match=ev['match']
-    ali_money=match.group(1)
-    record_path='E:/QQRobot/酷Q Pro_1/data/record/' #例：C:/QQRobot/酷Q Pro/data/record/
-    opener=urllib.request.build_opener()
-    opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+async def chat_alipay(bot, ev):
+    match = ev['match']
+    ali_money = match.group(1)
+    # 例：C:/QQRobot/酷Q Pro/data/record/
+    record_path = 'E:/QQRobot/酷Q Pro_1/data/record/'
+    opener = urllib.request.build_opener()
+    opener.addheaders = [
+        ('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
     urllib.request.install_opener(opener)
-    urllib.request.urlretrieve(f'https://mm.cqu.cc/share/zhifubaodaozhang/?money={ali_money}',f'{record_path}{ali_money}.mp3')
-    await bot.send(ev,f'[CQ:record,file={ali_money}.mp3]')
+    urllib.request.urlretrieve(
+        f'https://mm.cqu.cc/share/zhifubaodaozhang/?money={ali_money}', f'{record_path}{ali_money}.mp3')
+    await bot.send(ev, f'[CQ:record,file={ali_money}.mp3]')
     os.remove(f'{record_path}{ali_money}.mp3')
 
 
@@ -99,23 +106,27 @@ async def prey(session):
     pic = R.img(f"guru{random.randint(1, 2)}.jpg").cqcode
     await session.send(f"{pic}", at_sender=True)
 
+
 @sv.on_prefix('不许')
-async def chat_buxv(bot,ev):
-    m=ev.message
-    await bot.send(ev,f'不许不许{m}')
+async def chat_buxv(bot, ev):
+    m = ev.message
+    await bot.send(ev, f'不许不许{m}')
+
 
 @sv.on_prefix('禁止')
-async def chat_jinzhi(bot,ev):
-    m=ev.message
-    await bot.send(ev,f'禁止禁止{m}')
+async def chat_jinzhi(bot, ev):
+    m = ev.message
+    await bot.send(ev, f'禁止禁止{m}')
 
-@sv.on_prefix(('戳他','戳她','戳它'))
-async def chat_poke(bot,ev):
-    qq_nums_raw=str(ev.message)
-    qq_nums = re.findall(r'[1-9][0-9]{4,}',qq_nums_raw)
+
+@sv.on_prefix(('戳他', '戳她', '戳它'))
+async def chat_poke(bot, ev):
+    qq_nums_raw = str(ev.message)
+    qq_nums = re.findall(r'[1-9][0-9]{4,}', qq_nums_raw)
     for qq in qq_nums:
         await bot.send(ev, f'[CQ:poke,qq={qq}]')
 
-@sv.on_prefix('戳我')#后期会整合成一个，加一点复杂的新功能，现在先这么用
-async def chat_poke_self(bot,ev):
+
+@sv.on_prefix('戳我')  # 后期会整合成一个，加一点复杂的新功能，现在先这么用
+async def chat_poke_self(bot, ev):
     await bot.send(ev, f'[CQ:poke,qq={ev.user_id}]')
