433c9cbbaf142eeb1da1808a3896bf71a3478a06
Maximilian Wu <me@maxesisn.online>
2020-10-17 16:27:50 +0800

    添加config_example

---
+++ hoshino/config_example/priconne.py
@@ -0,0 +1,9 @@
+class arena:
+    AUTH_KEY = "1l45I4"
+
+class proxies:
+    enable = True
+    #此处自行修改为你需要使用的http proxy地址
+    http_proxy = 'http://127.0.0.1:8889'
+    https_proxy = 'http://127.0.0.1:8889'
+    socks_proxy = 'socks://127.0.0.1:8888'
