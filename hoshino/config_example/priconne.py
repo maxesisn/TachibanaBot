4d439b3d209235c24bf279a10946fbcca7b0a277
Maximilian Wu <me@maxesisn.online>
2020-10-17 16:36:40 +0800

    修复一处示例配置错误

--- hoshino/config_example/priconne.py
+++ hoshino/config_example/priconne.py
@@ -2,7 +2,7 @@ class arena:
     AUTH_KEY = "1l45I4"
 
 class proxies:
-    enable = True
+    enable = False
     #此处自行修改为你需要使用的http proxy地址
     http_proxy = 'http://127.0.0.1:8889'
     https_proxy = 'http://127.0.0.1:8889'
