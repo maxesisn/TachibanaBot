ddfaf5843908ed30e6ec9df566d9e76a83320ff9
Maximilian Wu <me@maxesisn.online>
2020-10-18 19:55:04 +0800

    去除chat_alipay中的无用代码

--- hoshino/modules/groupmaster/chat.py
+++ hoshino/modules/groupmaster/chat.py
@@ -107,17 +107,7 @@ async def new_year_burst(bot, ev):
 async def chat_alipay(bot, ev):
     match = ev['match']
     ali_money = match.group(1)
-    # 例：/root/Bot/res/voice/
-    record_path = '/root/Bot/res/voice/'
-    opener = urllib.request.build_opener()
-    opener.addheaders = [
-        ('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
-    urllib.request.install_opener(opener)
-    urllib.request.urlretrieve(
-        f'https://mm.cqu.cc/share/zhifubaodaozhang/?money={ali_money}',
-        f'{record_path}{ali_money}.mp3')
-    await bot.send(ev, f'[CQ:record,file=file:///{record_path}{ali_money}.mp3]')
-    os.remove(f'{record_path}{ali_money}.mp3')
+    await bot.send(ev, f'[CQ:record,file=https://mm.cqu.cc/share/zhifubaodaozhang/?money={ali_money}]')
 
 
 @sv.on_prefix('不许')
