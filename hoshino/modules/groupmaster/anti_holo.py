cb1b5fdbfdfed393bf089aa9ca23d4c66b7ffc49
Maximilian Wu <me@maxesisn.online>
2020-10-20 15:33:58 +0800

    暂时性data update，后续继续观察

--- hoshino/modules/groupmaster/anti_holo.py
+++ hoshino/modules/groupmaster/anti_holo.py
@@ -52,21 +52,15 @@ holo ホロ 木口 术口
 萝卜子 ロボ子さん Roboko 大根子
 樱巫女 さくらみこ Sakura Miko 樱火龙 elite工厂厂长 黄油精英巫女 赌鬼巫女 樱污女
 星街彗星 星街 すいせい Hoshimati Suisei 星姐 阿星 彗彗 suisui 有点神经病的蓝发大姐姐 有点大姐姐的蓝发神经病
-夜空梅露 夜空メル Yozora Mel 梅露 梅球王 Banpire 清楚系天才美少女吸血鬼
 夏色祭 夏色まつり Natsuiro Matsuri 马自立 祭妹 夏哥 夏半首
 赤井心 赤井はあと Akai Haato 心心 心大人 はぁちゃま 哈恰玛 哈恰嘛
 亚绮 罗森塔尔 アキ ローゼンタール Aki Rosenthal akirose 李姐 力速双A弱精灵
 白上吹雪 白上フブキ Shirakami Fubuki 🌽 fbk 小狐狸 屑狐狸 喵喵狐 debuki 玉米人 赫鲁晓狐 工具狐
 人见酷丽丝 人見クリス Hitomi Kurisu
-凑阿库娅 湊あくあ Minato Aqua 阿库娅 洋葱 阿夸 夸哥 夸神 海王 山田赫敏 大亏哥 桐谷夸人 
 紫咲诗音 紫咲シオン Murasaki Shion 诗音 傻紫 小学生 紫咲紫苑
 愈月巧可 癒月ちょこ Yuzuki Choco 巧可老师
-大空昴 大空スバル Ōzora Subaru 古神 
 大神澪 大神ミオ Ōkami Mio 大神三才 二哈 三才妈妈
-猫又小粥 猫又おかゆ Nekomata Okayu 小粥 阿汤 汤哥 饭团猫 大脸猫
 戌神 沁音 戌神ころね Inugami Korone 吼啦迷迭吼啦哟 面包狗 外神
-兔田佩克拉 兎田ぺこら Usada Pekora 佩克拉 佩可拉 屑兔子 
-润羽露西娅 潤羽るしあ Uruha Rushia 死灵魔法师 死灵使 绿粽子 三亚王 狂爆粽子 阿马粽
 不知火芙蕾雅 不知火フレア Shiranui Flare 芙蕾雅 芙碳 阿火 ぬいぬい nuinui フーたん
 白银诺艾尔 白銀ノエル Shirogane Noel 
 宝钟 玛琳 宝鐘マリン Houshou Marine 无船承运人 山贼
@@ -74,8 +68,6 @@ holo ホロ 木口 术口
 桐生可可 ココ Kiryū Kiryuu Coco 🐉 蝗 虫皇 龙皇 憨憨龙 ass龙 宝批龙 西成女王
 角卷绵芽 角巻わため Tsunomaki Watame 毛球 顶顶羊 咚咚羊 畜生羊 木口锤石
 常暗永远 常闇トワ Tokoyami Towa 永远大人 トワ様
-姬森璐娜 姫森 ルーナ Himemori Luuna 璐娜 英语教师
-雪花菈米 雪花ラミィYukihana Lamy 菈米 雪花纯生 雪妈 菈米妈妈
 桃铃音音 桃鈴ねね Momosuzu Nene 铃桃音音
 狮白牡丹 獅白ぼたん Shishiro Botan 狮白
 魔乃阿萝耶 魔乃アロエ Mano Aloe 魔乃 阿萝耶
