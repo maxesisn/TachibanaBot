a20af54ff413448d07dd247240de45ebcf74ca7f
Maximilian Wu <me@maxesisn.online>
2020-11-10 08:26:05 +0800

    add custom functions to 'chat.py'

--- hoshino/modules/groupmaster/chat.py
+++ hoshino/modules/groupmaster/chat.py
@@ -4,6 +4,7 @@ from nonebot import on_command
 
 from hoshino import R, Service, priv, util
 from hoshino.config.__bot__ import SUPERUSERS
+import re
 
 
 # basic function for debug, not included in Service('chat')
@@ -102,3 +103,53 @@ nyb_player = f'''{R.img('newyearburst.gif').cqcode}
 async def new_year_burst(bot, ev):
     if random.random() < 0.02:
         await bot.send(ev, nyb_player)
+
+@sv.on_rex(r'^给我(\d+\.?\d*)块')
+@sv.on_rex(r'^我想要(\d+\.?\d*)块')
+async def chat_alipay(bot, ev):
+    match = ev['match']
+    ali_money = match.group(1)
+    await bot.send(ev, f'[CQ:record,file=https://mm.cqu.cc/share/zhifubaodaozhang/?money={ali_money}]')
+
+
+@sv.on_prefix('不许')
+async def chat_buxv(bot, ev):
+    m = ev.message
+    await bot.send(ev, f'不许不许{m}')
+
+
+@sv.on_prefix('禁止')
+async def chat_jinzhi(bot, ev):
+    m = ev.message
+    await bot.send(ev, f'禁止禁止{m}')
+
+
+@sv.on_prefix(('戳他', '戳她', '戳它'))
+async def chat_poke(bot, ev):
+    qq_nums_raw = str(ev.message)
+    qq_nums = re.findall(r'[1-9][0-9]{4,}', qq_nums_raw)
+    for qq in qq_nums:
+        await bot.send(ev, f'[CQ:poke,qq={qq}]')
+
+
+@sv.on_prefix('戳我')  # 后期会整合成一个，加一点复杂的新功能，现在先这么用
+async def chat_poke_self(bot, ev):
+    await bot.send(ev, f'[CQ:poke,qq={ev.user_id}]')
+
+
+@sv.on_prefix(('说：', '说:'))
+async def chat_speak(bot, ev):
+    await bot.send(ev, f'[CQ:tts,text={ev.message}]')
+
+
+@sv.on_keyword(('给点礼物', '我也要礼物'))
+async def chat_gift(bot, ev):
+    await bot.send(ev, f'[CQ:gift,qq={ev.user_id},id={random.randint(0,8)}]')
+
+
+@sv.on_keyword(('色图', '涩图'),only_to_me=True)
+async def chat_setu(bot, ev):
+    if ev.raw_message.startswith('橘小姐'):
+        pass
+    else:
+        await bot.send(ev, R.img('no-H.JPG').cqcode)
