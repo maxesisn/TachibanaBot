400ef27b1e86449344fac7b4bd72be32080ee5b6
Maximilian Wu <me@maxesisn.online>
2020-11-24 14:24:41 +0800

    support broadcasting CQcode

--- hoshino/modules/botmanage/broadcast.py
+++ hoshino/modules/botmanage/broadcast.py
@@ -14,6 +14,7 @@ async def broadcast(session: CommandSession):
         for g in gl:
             await asyncio.sleep(0.5)
             try:
+                msg=CQ_trans(msg)
                 await session.bot.send_group_msg(self_id=sid, group_id=g, message=msg)
                 hoshino.logger.info(f'群{g} 投递广播成功')
             except Exception as e:
@@ -23,3 +24,11 @@ async def broadcast(session: CommandSession):
                 except Exception as e:
                     hoshino.logger.critical(f'向广播发起者进行错误回报时发生错误：{type(e)}')
     await session.send(f'广播完成！')
+
+
+def CQ_trans(cqcode:str) -> str:
+    CQcode = cqcode
+    CQcode = CQcode.replace('&amp;','&')
+    CQcode = CQcode.replace('&#91;','[')
+    CQcode = CQcode.replace('&#93;',']')
+    return CQcode
